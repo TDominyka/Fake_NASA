491f48bfde5c2a3cc34c67e2b5adcbb669596765
TDominyka <d.razanovaite@gmail.com>
2021-09-15 02:47:46 +0300

    Added changing images and video

--- js/animations.js
+++ js/animations.js
@@ -1,9 +1,26 @@
-var myVideo = document.getElementById("video"); 
+"use strict";
+ 
+setInterval(newsImg, 1000);
+var src=["https://www.nasa.gov/sites/default/files/thumbnails/image/orbital.jpg","media/nasa-logo.svg","media/share.svg"];
+var i=0;
+
+function newsImg() {
+	document.getElementById("first-img").src = src[i];
+	if(i==2)
+		i=0;
+	else
+		i+=1;
+}
 
 function playPause() { 
-	console.log("paspaudziau");
-  if (myVideo.paused) 
-    myVideo.play(); 
-  else 
-    myVideo.pause(); 
-}
+	var myVideo = document.getElementById("video");
+	var myButton = document.getElementById("play_butt_img");
+	if (myVideo.paused){ 
+		myVideo.play(); 
+		myButton.style.visibility="hidden";
+	}
+	else {
+		myVideo.pause(); 
+		myButton.style.visibility="visible";
+	}
+};
