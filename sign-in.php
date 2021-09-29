706f9a58d2622c029a677c6d67ec39e729d8b8cd
Parmenion Charistos <parmenion.char@gmail.com>
2021-09-29 05:39:03 +0200

    styled the sign-in page

--- sign-in.php
+++ sign-in.php
@@ -4,6 +4,7 @@
 		<title>Fake NASA Admin Page</title>
 		<meta charset="UTF-8">
 		<meta name="author" content="Dominyka Razanovaitė and Parmenion Charistos">
+		<link rel="stylesheet" href="style/sign-in.css">
 	</head>
 	<body>
 		<?php
@@ -18,11 +19,15 @@
 			}
 		?>
 		<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
-			<label for="username">Username:</label>
-			<input type="text" name="username" id="username"></input>
-			<label for="password">Password:</label>
-			<input type="password" name="password" id="password"></input>
-			<input type="submit"></input>
+			<div class="grid" id="sign-in-grid">
+				<div class="grid-item" id="logo"><img src="media/nasa-logo.svg" alt="NASA logo"></img></div>
+				<div class="grid-item" id="title"><h1>Sign in</h1></div>
+				<label class="grid-item" for="username" id="username-label">Username</label>
+				<input class="grid-item" type="text" name="username" id="username" placeholder="e.g. IamMrNimbus"></input>
+				<label class="grid-item" for="password" id="password-label">Password</label>
+				<input class="grid-item" type="password" name="password" id="password" placeholder="e.g. NotYourBirthDate"></input>
+				<input class="grid-item" type="submit" id="submit-button"></input>
+			</div>
 		</form>
 	</body>
 </html>
